540c5d166e8a9ce0ae93995d9a1545801fc94f47
pi <yangfuy@gmail.com>
2020-06-01 19:27:39 -0400

    Installed Tensorflow, opencv and numpy

---
+++ Visual.py
@@ -0,0 +1,7 @@
+import cv2
+import numpy as  np
+import tensorflow as tf
+
+print ("OpenCV version:\t"+ cv2.__version__)
+print ("Numpy version:\t"+ np.__version__)
+print ("TF version:\t"+ tf.__version__)
